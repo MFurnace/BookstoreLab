c0fa440f561065ab1c90efdba895d81b8d71de49
MFurnace <mfurnace1@gamil.com>
2020-05-14 17:16:36 -0400

    first commit

---
+++ UserDefinedClasses.java
@@ -0,0 +1,27 @@
+public class Car
+{
+    public String Color;
+    public int NumberOfDoors;
+
+    public void start()
+    {
+        System.out.println("Car starting");
+    }
+
+    public void stop()
+    {
+        System.out.println("Car stopping");
+    }
+
+    public void drive()
+    {
+        System.out.println("Car driving");
+    }
+}
+
+// instantiating and using a car object
+Car car = new Car();
+car.Color = "blue";
+car.NumberOfDoors = 4;
+car.start();
+car.drive();
